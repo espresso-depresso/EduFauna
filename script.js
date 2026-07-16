d557f0069ada12a8cf555d2c433ba1f2a0bd054c
aleanny <aleanny.olivero@gmail.com>
2026-07-16 14:26:55 -0400

    agregar fonts nuevos

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const modeloRana=document.getElementById("modeloRana");
 const sections = Array.from(document.querySelectorAll("section"));
 
-const shiftPositions=[25,-20,0,25];
+//POSICIONES DENTRO DE LAS SECCIONES NO TOCAR
+const shiftPositions=[25,-25,0,25];
+//POSICIONES DE LOS ANGULOS NO TOCAR
 const cameraOrbits=[[135,90],[135,90],[-180,0],[45,90]];
 
 const interpolate =(start,end,progress)=> start+(end-start)*progress;
